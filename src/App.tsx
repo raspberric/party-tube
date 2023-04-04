1921866d8f2fad2b2cdbe0e9b40bb757739a3d8a
Nikola Malinovic <nikolamalinovic42@gmail.com>
2023-04-04 02:39:55 +0200

    Move HomeScreen to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Button, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
+import { HomeScreen } from './pages/HomeScreen';
 
 export const App: React.FC = () => {
   return (
@@ -16,61 +15,4 @@ export const App: React.FC = () => {
 const AppWrapper = styled.div`
   width: 100vw;
   min-height: 100vh;
-`;
-
-const HomeScreen: React.FC = () => {
-  const [nickname, setNickname] = useState('');
-  const navigate = useNavigate();
-
-  return (
-    <HomeScreenWrapper>
-      <TextField
-        onChange={(event) => setNickname(event.target.value)}
-        label="Enter your nickname"
-        variant="outlined"
-      />
-      <StyledWhatAreYou />
-      <StyledButton
-        disabled={!nickname}
-        onClick={() => navigate('host')}
-        variant="contained"
-      >
-        Host
-      </StyledButton>
-      <StyledButton
-        disabled={!nickname}
-        onClick={() => navigate('guest')}
-        variant="contained"
-        color="secondary"
-      >
-        Guest
-      </StyledButton>
-    </HomeScreenWrapper>
-  );
-};
-
-const WhatAreYou: React.FC<{ className?: string }> = ({ className }) => (
-  <Typography className={className}>What are you?</Typography>
-);
-const StyledWhatAreYou = styled(WhatAreYou)`
-  font-size: 2em;
-`;
-
-const StyledButton = styled(Button)`
-  height: 50px;
-`;
-
-const HomeScreenWrapper = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  align-content: center;
-  padding: 20px;
-
-  ${StyledWhatAreYou} {
-    margin-top: 60px;
-    margin-bottom: 30px;
-  }
-  ${StyledButton} {
-    margin: 20px 0;
-  }
 `;
---
+++ src/pages/HomeScreen.tsx
@@ -0,0 +1,61 @@
+import { Button, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const HomeScreen: React.FC = () => {
+  const [nickname, setNickname] = useState('');
+  const navigate = useNavigate();
+
+  return (
+    <HomeScreenWrapper>
+      <TextField
+        onChange={(event) => setNickname(event.target.value)}
+        label="Enter your nickname"
+        variant="outlined"
+      />
+      <StyledWhatAreYou />
+      <StyledButton
+        disabled={!nickname}
+        onClick={() => navigate('host')}
+        variant="contained"
+      >
+        Host
+      </StyledButton>
+      <StyledButton
+        disabled={!nickname}
+        onClick={() => navigate('guest')}
+        variant="contained"
+        color="secondary"
+      >
+        Guest
+      </StyledButton>
+    </HomeScreenWrapper>
+  );
+};
+
+const WhatAreYou: React.FC<{ className?: string }> = ({ className }) => (
+  <Typography className={className}>What are you?</Typography>
+);
+const StyledWhatAreYou = styled(WhatAreYou)`
+  font-size: 2em;
+`;
+
+const StyledButton = styled(Button)`
+  height: 50px;
+`;
+
+const HomeScreenWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-content: center;
+  padding: 20px;
+
+  ${StyledWhatAreYou} {
+    margin-top: 60px;
+    margin-bottom: 30px;
+  }
+  ${StyledButton} {
+    margin: 20px 0;
+  }
+`;
