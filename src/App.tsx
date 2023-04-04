6fc238d334bfff809e9a9696961844cd2d155d00
Nikola Malinovic <nikolamalinovic42@gmail.com>
2023-04-04 05:59:08 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 import { HomeScreen } from './pages/HomeScreen';
+import { HostPage } from './pages/HostPage';
 
 export const App: React.FC = () => {
   return (
     <AppWrapper>
       <Routes>
         <Route path="/" element={<HomeScreen />}></Route>
+        <Route path="/host/:nickname" element={<HostPage />}></Route>
+        <Route path="/guest" element={<HostPage />}></Route>
       </Routes>
     </AppWrapper>
   );
